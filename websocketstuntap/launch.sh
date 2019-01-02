e83797f8d393446e81488294c6081b2dcd1e6ebf
killinux <killinux@163.com>
2019-01-02 12:05:19 +0000

    if not ifconfig hw ,the ip of br1 will autochange with the ip of tapX

--- websocketstuntap/launch.sh
+++ websocketstuntap/launch.sh
@@ -12,6 +12,8 @@
 #brctl stp br1 on
 #ip link set br1 promisc on 
 #ip link set br1 up
+#ifconfig br1 hw ether ee:ee:ee:ee:ee:50
+#if not ifconfig hw ,the ip of br1 will autochange with the ip of tapX
 #ifconfig br1 10.0.2.1 netmask 255.0.0.0 up
 #
 #dnsmasq --strict-order --except-interface=lo --interface=br1 --listen-address=10.0.2.1 --bind-interfaces  --dhcp-range=10.0.2.100,10.0.2.254 --conf-file=""  --pid-file=/var/run/qemu-dhcp-br1.pid  --dhcp-leasefile=/var/run/qemu-dhcp-br1.leases --dhcp-no-override
@@ -21,6 +23,6 @@
 #virtualenv mysite
 #source mysite/bin/activate
 #pip install mod_pywebsocket
-kill `pidof python`
-source /opt/qemu/deobfuscated/websocketstuntap/mysite/bin/activate
+#kill `pidof python`
+#source /opt/qemu/deobfuscated/websocketstuntap/mysite/bin/activate
 python -m mod_pywebsocket.standalone -d . --log-level=info -p 3000
